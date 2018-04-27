51722489dd7ef8939fad89cc4b61e8fac94c3424
Roniece A Ricardo <roniecericardo@gmail.com>
2018-04-27 00:23:25 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -18,7 +18,7 @@ end
 def times_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   number_of_times.times do
-    print phrase
+    puts phrase
   end
 end
 
